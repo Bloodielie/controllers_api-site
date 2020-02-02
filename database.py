b1d408f256459fe40d342cb86fbdc67ce7d73b63
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2020-02-02 16:05:53 +0300

    Add files via upload

--- database.py
+++ database.py
@@ -1,15 +1,23 @@
 import orm
 import sqlalchemy
 from databases import Database
-from config import url_bd, tb_name_1, tb_name_2
+from config import url_bd, tb_name_brest_dirty, tb_name_brest_clean, tb_name_gomel_dirty, tb_name_gomel_clean, tb_name_grodno_dirty, tb_name_grodno_clean
 
 
 database = Database(url_bd)
 metadata = sqlalchemy.MetaData()
 
 
-class BusStopDirty(orm.Model):
-    __tablename__ = tb_name_1
+class BusStopDirty_Brest(orm.Model):
+    __tablename__ = tb_name_brest_dirty
+    __database__ = database
+    __metadata__ = metadata
+    id = orm.Integer(primary_key=True)
+    bus_stop = orm.String(max_length=100)
+    time = orm.Integer()
+
+class BusStopClear_Brest(orm.Model):
+    __tablename__ = tb_name_brest_clean
     __database__ = database
     __metadata__ = metadata
     id = orm.Integer(primary_key=True)
@@ -17,8 +25,33 @@ class BusStopDirty(orm.Model):
     time = orm.Integer()
 
 
-class BusStopClear(orm.Model):
-    __tablename__ = tb_name_2
+class BusStopDirty_Grodno(orm.Model):
+    __tablename__ = tb_name_grodno_dirty
+    __database__ = database
+    __metadata__ = metadata
+    id = orm.Integer(primary_key=True)
+    bus_stop = orm.String(max_length=100)
+    time = orm.Integer()
+
+class BusStopClear_Grodno(orm.Model):
+    __tablename__ = tb_name_grodno_clean
+    __database__ = database
+    __metadata__ = metadata
+    id = orm.Integer(primary_key=True)
+    bus_stop = orm.String(max_length=100)
+    time = orm.Integer()
+
+
+class BusStopDirty_Gomel(orm.Model):
+    __tablename__ = tb_name_gomel_dirty
+    __database__ = database
+    __metadata__ = metadata
+    id = orm.Integer(primary_key=True)
+    bus_stop = orm.String(max_length=100)
+    time = orm.Integer()
+
+class BusStopClear_Gomel(orm.Model):
+    __tablename__ = tb_name_gomel_clean
     __database__ = database
     __metadata__ = metadata
     id = orm.Integer(primary_key=True)
