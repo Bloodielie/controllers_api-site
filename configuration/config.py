2b956f1edd5b9516460e9801e753b28a1c8fab14
BuildTools <unconfigured@null.spigotmc.org>
2020-03-08 23:22:50 +0300

    add config

--- configuration/config.py
+++ configuration/config.py
@@ -1,6 +1,24 @@
 from utils.json import get_json
+from starlette.config import Config
+from starlette.templating import Jinja2Templates
+
+config = Config(".env")
+
+SECRET_KEY = config('SECRET_KEY', cast=str)
+ALGORITHM = config('ALGORITHM', cast=str)
+ACCESS_TOKEN_EXPIRE_MINUTES = 7200
+
+login_vk: str = config('login_vk', cast=str)
+password_vk: str = config('password_vk', cast=str)
+
+login_email: str = config('login_email', cast=str)
+password_email: str = config('password_email', cast=str)
+
+url_bd: str = config('url_bd', cast=str)
 
 path_to_json = '../fastapi/json'
+
+templates = Jinja2Templates(directory="../fastapitest — копия/templates")
 
 stop_bus_brest: list = get_json(f'{path_to_json}/stopbus_brest.json')
 stop_bus_gomel: list = get_json(f'{path_to_json}/stopbus_gomel.json')
@@ -14,16 +32,12 @@ trolleybusesstop_brest: dict = get_json(f'{path_to_json}/trolleybusesstop_brest.
 trolleybusesstop_gomel: dict = get_json(f'{path_to_json}/trolleybusesstop_gomel.json')
 trolleybusesstop_grodno: dict = get_json(f'{path_to_json}/trolleybusesstop_grodno.json')
 
-url_bd: str = 'postgresql://postgres:1234@localhost/bus_stop'
 tb_name_brest_dirty: str = "bus_stop_dirty"
 tb_name_brest_clean: str = "bus_stop_clear"
 tb_name_gomel_dirty: str = "bus_stop_dirty_gomel"
 tb_name_gomel_clean: str = "bus_stop_clean_gomel"
 tb_name_grodno_dirty: str = "bus_stop_dirty_grodno"
 tb_name_grodno_clean: str = "bus_stop_clean_grodno"
-
-login: str = ''
-password: str = ''
 
 clean_dirty_word: list = ['чисто', 'как', 'актуально?', 'cтоят на']
 clean_clean_word: list = ['чисто', 'стерильно', 'чистота', 'чистенько']
