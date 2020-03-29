f8c6db3b428a35275c5d9a8d39607b778e9e7203
Bloodielie <riopro2812@gmail.com>
2020-03-29 19:58:16 +0300

    fix main

--- main.py
+++ main.py
@@ -64,4 +64,4 @@ async def shutdown() -> None:
 
 if __name__ == "__main__":
     port = os.environ.get('PORT')
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("main:app", host="0.0.0.0", port=str(port), log_level="info")
