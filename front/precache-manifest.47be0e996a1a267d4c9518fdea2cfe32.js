8786ee393c2e1d4db528994921238b632039a395
Bloodielie <49410211+Bloodielie@users.noreply.github.com>
2020-04-19 01:14:22 +0300

    Add files via upload

---
+++ front/precache-manifest.47be0e996a1a267d4c9518fdea2cfe32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5befc8fdbe227ae456ab99e32a255371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b395980e5dc05964961",
+    "url": "/static/css/main.2791393b.chunk.css"
+  },
+  {
+    "revision": "28e29faed4365cece0c8",
+    "url": "/static/js/2.b2ee2ff4.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.b2ee2ff4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b395980e5dc05964961",
+    "url": "/static/js/main.3ac1b894.chunk.js"
+  },
+  {
+    "revision": "f5d7094a0c0b73bc5b18",
+    "url": "/static/js/runtime-main.11d93355.js"
+  }
+]);
